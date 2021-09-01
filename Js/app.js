98a41153451f8b173c9c4a9f78046e47fc723c81
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2021-09-01 15:58:04 +0530

    work page added - initial commit

--- Js/app.js
+++ Js/app.js
@@ -4,17 +4,22 @@ const navLinks = document.querySelectorAll('.navbar__link');
 const navListItems = document.querySelectorAll('.navbar__item');
 const navBtn = document.querySelector('.btn-work');
 const logo = document.querySelector('.logo-item');
+const logoWork = document.querySelector('.logo-work')
 
 const homeBtn = document.querySelector('#home-btn');
 const skillsBtn = document.querySelector('#skills-btn');
 const educationBtn = document.querySelector('#education-btn');
 const contactBtn = document.querySelector('.cta__btn-contact');
 
+const body = document.getElementsByTagName('body')[0];
+console.log(body);
+console.log(body.id);
 
 
 window.addEventListener('scroll', () => {
     const scroll = window.scrollY;
     // console.log(scroll);
+    console.log(logoWork)
 
     if (scroll > 0) {
         navbar.classList.add('scrolled');
@@ -32,7 +37,12 @@ window.addEventListener('scroll', () => {
 
         navBtn.classList.add('nav-btn-scrolled');
 
-        logo.src = "./Assets/logo-name-scrolled.svg"
+        if (body.id === 'work-page') {
+            logoWork.src = "../Assets/logo-name-scrolled.svg"
+        } else {
+            logo.src = "./Assets/logo-name-scrolled.svg"
+        }
+
     } else {
         navbar.classList.remove('scrolled');
         navLinks.forEach(link => {
@@ -45,26 +55,40 @@ window.addEventListener('scroll', () => {
 
         navBtn.classList.remove('nav-btn-scrolled');
 
-        logo.src = "./Assets/logo-name-svg.svg"
+
+        if (body.id === 'work-page') {
+            logoWork.src = "../Assets/logo-name-normal.svg"
+        } else {
+            logo.src = "./Assets/logo-name-normal.svg"
+        }
+
+        
     }
 })
 
-homeBtn.addEventListener('click', () => {
-    window.scrollBy(0, 0);
-    console.log(window.scrollY);
-});
+if (body.id !== 'work-page') {
+    homeBtn.addEventListener('click', () => {
+        window.scroll(0, 0);
+        console.log(window.scrollY);
+    });
+    
+    skillsBtn.addEventListener('click', () => {
+        window.scroll(0, 720);
+        console.log(window.scrollY);
+    
+        // const skillSection = document.getElementById('skills');
+        // console.log(skillSection);
+        // skillSection.scrollIntoView();
+    });
+    
+    educationBtn.addEventListener('click', () => {
+        window.scrollBy(0, 1440);
+        console.log(window.scrollY);
+    });
+    
+    contactBtn.addEventListener('click', () => {
+        window.scroll(0, 2170);
+        console.log(window.scrollY);
+    });
+}
 
-skillsBtn.addEventListener('click', () => {
-    window.scrollBy(0, 720);
-    console.log(window.scrollY);
-});
-
-educationBtn.addEventListener('click', () => {
-    window.scrollBy(0, 1440);
-    console.log(window.scrollY);
-});
-
-contactBtn.addEventListener('click', () => {
-    window.scrollBy(0, 2170);
-    console.log(window.scrollY);
-});
