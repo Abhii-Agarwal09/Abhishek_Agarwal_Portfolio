8b35acdbb2ae4634da718b9b2992b600031d88e3
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2021-10-31 20:56:04 +0530

    lazy loading of sections added

--- v2/script.js
+++ v2/script.js
@@ -1,4 +1,28 @@
-window.onload = function homePageReveal () {
+window.onload = function homePageReveal() {
     const homePage = document.querySelector('.main-content');
     homePage.classList.add('active');
-}
+};
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.35,
+};
+
+const observerCallback = function (entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+};
+
+let observer = new IntersectionObserver(observerCallback, options);
+
+let connectWithMeSection = document.querySelector('.contact-social--container');
+let targets = document.querySelectorAll('.section');
+
+observer.observe(connectWithMeSection);
+targets.forEach((target) => {
+    observer.observe(target);
+});
